40bd348ae6e40dd29373921c06934516d9a9182a
VINICIUS PORTO ALMEIDA <vinicius.almeida@bemol.local>
2021-03-22 01:01:12 -0400

    routes

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -6,11 +6,11 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 
 export default class UsersController {
   public async create(req: Request, res: Response): Promise<Response> {
-    const { name, email, password } = req.body;
+    const { name, email, password, user_type } = req.body;
 
     const createUser = container.resolve(CreateUserService);
 
-    const user = await createUser.execute({ name, email, password });
+    const user = await createUser.execute({ name, email, password, user_type });
 
     return res.json(classToClass(user));
   }
