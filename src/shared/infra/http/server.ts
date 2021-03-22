a56c98004289638d67dd8cf521b7c99797c30e96
VINICIUS PORTO ALMEIDA <vinicius.almeida@bemol.local>
2021-03-21 20:30:49 -0400

    orm2

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -17,7 +17,7 @@ import '../../container';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(rateLimiter);
