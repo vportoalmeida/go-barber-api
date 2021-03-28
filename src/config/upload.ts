8b4c522a2b63f2edb48d9b689c88bf89f5ed19e8
VINICIUS PORTO ALMEIDA <vinicius.almeida@bemol.local>
2021-03-28 18:36:54 -0400

    try

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -32,7 +32,7 @@ export default {
     storage: diskStorage({
       destination: tempFolder,
       filename(req, file, callback) {
-        const fileHash = randomBytes(10).toString('HEX');
+        const fileHash = randomBytes(10).toString('hex');
         const fileName = `${fileHash}-${file.originalname}`;
 
         return callback(null, fileName);
